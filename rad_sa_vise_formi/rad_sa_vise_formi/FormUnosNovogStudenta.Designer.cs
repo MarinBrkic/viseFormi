0fd86d6715e429710b97981abdae019237766a47
MarinBrkic <152974525+MarinBrkic@users.noreply.github.com>
2024-10-11 17:18:50 +0200

    Add files via upload

---
+++ rad_sa_vise_formi/rad_sa_vise_formi/FormUnosNovogStudenta.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace rad_sa_vise_formi
+{
+    partial class FormUnosNovogStudenta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxPrezime = new System.Windows.Forms.TextBox();
+            this.textBoxIme = new System.Windows.Forms.TextBox();
+            this.comboBoxSmjer = new System.Windows.Forms.ComboBox();
+            this.textBoxBrojIndeksa = new System.Windows.Forms.TextBox();
+            this.dateTimePickerDatumRodjenja = new System.Windows.Forms.DateTimePicker();
+            this.buttonSnimi = new System.Windows.Forms.Button();
+            this.buttonOtkazi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ime:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Prezime";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Smjer";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(221, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Datum rodjenja:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(221, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Broj indeksa:";
+            // 
+            // textBoxPrezime
+            // 
+            this.textBoxPrezime.Location = new System.Drawing.Point(11, 71);
+            this.textBoxPrezime.Name = "textBoxPrezime";
+            this.textBoxPrezime.Size = new System.Drawing.Size(182, 22);
+            this.textBoxPrezime.TabIndex = 5;
+            // 
+            // textBoxIme
+            // 
+            this.textBoxIme.Location = new System.Drawing.Point(15, 28);
+            this.textBoxIme.Name = "textBoxIme";
+            this.textBoxIme.Size = new System.Drawing.Size(178, 22);
+            this.textBoxIme.TabIndex = 6;
+            // 
+            // comboBoxSmjer
+            // 
+            this.comboBoxSmjer.FormattingEnabled = true;
+            this.comboBoxSmjer.Items.AddRange(new object[] {
+            "Programer",
+            "Sistem Administrator",
+            "Mrežni Administrator"});
+            this.comboBoxSmjer.Location = new System.Drawing.Point(11, 115);
+            this.comboBoxSmjer.Name = "comboBoxSmjer";
+            this.comboBoxSmjer.Size = new System.Drawing.Size(182, 24);
+            this.comboBoxSmjer.TabIndex = 7;
+            // 
+            // textBoxBrojIndeksa
+            // 
+            this.textBoxBrojIndeksa.Location = new System.Drawing.Point(217, 28);
+            this.textBoxBrojIndeksa.Name = "textBoxBrojIndeksa";
+            this.textBoxBrojIndeksa.Size = new System.Drawing.Size(200, 22);
+            this.textBoxBrojIndeksa.TabIndex = 8;
+            // 
+            // dateTimePickerDatumRodjenja
+            // 
+            this.dateTimePickerDatumRodjenja.Location = new System.Drawing.Point(217, 73);
+            this.dateTimePickerDatumRodjenja.Name = "dateTimePickerDatumRodjenja";
+            this.dateTimePickerDatumRodjenja.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePickerDatumRodjenja.TabIndex = 9;
+            // 
+            // buttonSnimi
+            // 
+            this.buttonSnimi.Location = new System.Drawing.Point(11, 160);
+            this.buttonSnimi.Name = "buttonSnimi";
+            this.buttonSnimi.Size = new System.Drawing.Size(101, 31);
+            this.buttonSnimi.TabIndex = 10;
+            this.buttonSnimi.Text = "Snimi";
+            this.buttonSnimi.UseVisualStyleBackColor = true;
+            this.buttonSnimi.Click += new System.EventHandler(this.buttonSnimi_Click);
+            // 
+            // buttonOtkazi
+            // 
+            this.buttonOtkazi.Location = new System.Drawing.Point(247, 160);
+            this.buttonOtkazi.Name = "buttonOtkazi";
+            this.buttonOtkazi.Size = new System.Drawing.Size(96, 31);
+            this.buttonOtkazi.TabIndex = 11;
+            this.buttonOtkazi.Text = "Otkazi";
+            this.buttonOtkazi.UseVisualStyleBackColor = true;
+            this.buttonOtkazi.Click += new System.EventHandler(this.buttonOtkazi_Click);
+            // 
+            // FormUnosNovogStudenta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 237);
+            this.Controls.Add(this.buttonOtkazi);
+            this.Controls.Add(this.buttonSnimi);
+            this.Controls.Add(this.dateTimePickerDatumRodjenja);
+            this.Controls.Add(this.textBoxBrojIndeksa);
+            this.Controls.Add(this.comboBoxSmjer);
+            this.Controls.Add(this.textBoxIme);
+            this.Controls.Add(this.textBoxPrezime);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormUnosNovogStudenta";
+            this.Text = "FormUnosNovogStudenta";
+            this.Load += new System.EventHandler(this.FormUnosNovogStudenta_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxPrezime;
+        private System.Windows.Forms.TextBox textBoxIme;
+        private System.Windows.Forms.ComboBox comboBoxSmjer;
+        private System.Windows.Forms.TextBox textBoxBrojIndeksa;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDatumRodjenja;
+        private System.Windows.Forms.Button buttonSnimi;
+        private System.Windows.Forms.Button buttonOtkazi;
+    }
+}
---
+++ rad_sa_vise_formi/rad_sa_vise_formi/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rad_sa_vise_formi
+{
+    internal class Student
+    {
+        private string ime, prezime, brojIndeksa, smjer;
+
+        private DateTime datumRodjenja;
+
+        public Student() { }
+
+        public string Ime { get => ime; set => ime = value; }
+        public string Prezime { get => prezime; set => prezime = value; }
+        public string BrojIndeksa { get => brojIndeksa; set => brojIndeksa = value; }
+        public string Smjer { get => smjer; set => smjer = value; }
+        public DateTime DatumRodjenja { get => datumRodjenja; set => datumRodjenja = value; }
+
+        public override string ToString()
+
+        {
+
+            return ime + " " + prezime + " " +
+            brojIndeksa + " " +
+            datumRodjenja.ToShortDateString() + " " + smjer;
+
+        }
+    }
+}
